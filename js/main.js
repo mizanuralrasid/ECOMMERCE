eb9df23c30dc33474ddf5b087e5e822a9c1acf2e
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2024-06-16 13:40:04 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// start screen
+let letsStart = document.getElementById("letsStart");
+letsStart.onclick = function () {
+    window.location.href = 'createAccount';
+};
+
+let cart = document.getElementById("letsStart");
+letsStart.onclick = function () {
+    window.location.href = 'createAccount';
+};
+let news = document.getElementById("letsStart");
+letsStart.onclick = function () {
+    window.location.href = 'createAccount';
+};
+let profile = document.getElementById("letsStart");
+letsStart.onclick = function () {
+    window.location.href = 'createAccount';
+};
+
+let loginBtn = document.getElementById("loginBtn");
+loginBtn.onclick = function() {
+    window.location.href = '../login';
+};
+
+// home screen products button
+
+
+
+
